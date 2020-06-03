23d0228ce85ab8c8bcb32382ef646f54850abcb0
Marcin Krawczyk <marcinn.krawczyk@gmail.com>
2020-06-03 21:40:58 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,13 @@
+#What is this ?
+
+Sample chat application backend that holds data in memory.
+
+#How it works?
+
+It uses apollo server asn GraphQL provider and socket.io to manage real time notifications.
+
+# Setup
+
+`npm install`
+
+`node app.js`
---
+++ app.js
@@ -0,0 +1,112 @@
+const express = require("express");
+const { ApolloServer, gql, PubSub } = require("apollo-server-express");
+const socketIo = require("socket.io");
+const http = require("http");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+const pubsub = new PubSub();
+const MESSAGE_ADDED = "MESSAGE_ADDED";
+
+const mockMessages = Array.from(Array(1).keys()).map((e, index) => ({
+  text: "asdas" + index,
+  id: "test" + index,
+  author: "test",
+  userId: "anonymous",
+  date: Date.now(),
+}));
+
+const messages = mockMessages;
+const resolvers = {
+  Subscription: {
+    addedMessage: {
+      // Additional event labels can be passed to asyncIterator creation
+      subscribe: () => pubsub.asyncIterator([MESSAGE_ADDED]),
+    },
+  },
+  Query: {
+    messages: () => messages,
+  },
+  Mutation: {
+    addMessage: (_, data, test) => {
+      const message = data.message;
+      message.id = messages.length.toString();
+      message.date = Date.now();
+      messages.push(message);
+
+      pubsub.publish(MESSAGE_ADDED, { addedMessage: message });
+      io.sockets.emit("CHAT_MESSAGE_ADDED", message);
+      return {
+        success: true,
+        message,
+      };
+    },
+  },
+};
+
+const typeDefs = gql`
+  type ChatMessage {
+    id: String
+    text: String
+    author: String
+    date: Float
+    userId: String
+  }
+
+  input ChatMessageInput {
+    text: String
+    author: String
+    userId: String
+  }
+
+  type Book {
+    title: String
+    author: String
+  }
+
+  type AddMessageResponse {
+    success: Boolean!
+    message: ChatMessage
+  }
+
+  type Mutation {
+    addMessage2(input: String!): AddMessageResponse
+    addMessage(message: ChatMessageInput): AddMessageResponse
+  }
+
+  type Subscription {
+    addedMessage: ChatMessage
+  }
+
+  type Query {
+    messages: [ChatMessage]
+  }
+`;
+
+io.on("connection", (client) => {
+  client.on("ADD_CHAT_MESSAGE", (message) => {
+    message.id = messages.length.toString();
+    message.date = Date.now();
+    messages.push(message);
+
+    client.emit("CHAT_MESSAGE_ADDED", message);
+  });
+});
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+apolloServer.applyMiddleware({ app });
+
+server.listen(4000, function () {
+  console.log(
+    `🚀 Server ready at http://localhost:4000${apolloServer.graphqlPath}`
+  );
+  const host = server.address().address;
+  const port = server.address().port;
+
+  console.info("App listening at http://%s:%s", host, port);
+});
